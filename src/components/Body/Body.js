28fb0f5224295d2f142d4f21c6a5420e244d14b3
Mohammed Alam <mohammedal2144@gmail.com>
2022-04-23 02:56:34 -0400

    Intro page all complete

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -5,12 +5,32 @@ import './Body.css';
 const Body = () =>{
     return (
     <div>
-        <h1>Welcome!</h1>
+        <h1>Welcome To Imagine RIT!</h1>
         <div className='home-text'>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            <p>This is the RIT Society of Softare Engineer's Advent of Code.</p>
+            <p>If you've always wanted to try software Engineering but never knew where to start, you're in luck!
+                We'll help you out. Right here, we'll walk you through setting up a coding enviornment, along with a few
+                practice problems to get your feet wet.
+            </p>
         </div>
-        <div className='home-text'>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        <div className='home-text steps'>
+            <h2>Directions</h2>
+            <ol>
+                <li>Go to the <a href='https://code.visualstudio.com/'>VS Code Website</a> and just click recommended settings for installing the software</li>
+                <li>Once Vs Code is installed, run it.</li>
+                <li>Click on the extensions tab. (Image shown below) <br/><img className="step-img" width="70px" src='../images/step3.jpeg'/></li>
+                <li>Search and Download The Code Runner Extension <br/> <img className="step-img" width='300px' src='../images/step4_1.jpeg'/> <br/> <img  className='step-img' src='../images/step4_2.jpeg' width="500px" /> </li>
+                <li>Now go back to the home screen, and start a new file <br/> <img className="step-img" width="70px" src='../images/step5_1.jpeg'/> <br/> <img  className='step-img' src='../images/step5_2.jpeg' width="500px" /> </li>
+                <li>Call this file example.py</li>
+            </ol>
+        </div>
+        <div className='home-text steps'>
+            <h2>Examples</h2>
+            <ol>
+                <li>Here is an example of a Python function, Running this will print "Hello World" to your console <br/> <img className="step-img" src='../images/step7_1.jpeg' /></li>
+                <li>Click run on the top right, and click run python file <br/> <img className="step-img" src='../images/step8_1.jpeg' /></li>
+                <li>This should be your result <br/> <img className="step-img" src='../images/step9_1.jpeg' /></li>
+            </ol>
         </div>
     </div>
     );
